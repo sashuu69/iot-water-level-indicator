5d4a42bde1c281d09a069b2d3543d22c94789303
Sashwat K <sashwatk@Sashwats-MacBook-Pro.local>
2019-10-01 11:11:52 +0530

    new code

---
+++ tank-module/NodeMCU-code/NodeMCU-code.ino
@@ -0,0 +1,15 @@
+/*
+ * Projecr Name : IOT Based Water Level Indicator
+ * Author : Sashwat K & Vijitha V Nair
+ * Created on : 01 Oct 2019
+ * Last updated : 01 Oct 2019
+ */
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}

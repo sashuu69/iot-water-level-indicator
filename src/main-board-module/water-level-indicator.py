58cb5e66b13549c3bbe1d60c5eb28416fefd84ed
Sashwat K <sashwat0001@gmail.com>
2019-10-10 15:48:45 +0530

    defined class

--- src/main-board-module/water-level-indicator.py
+++ src/main-board-module/water-level-indicator.py
@@ -1,5 +1,6 @@
 # Main process code
 # Project name: IOT based Water level indicator
+# Repository link: https://github.com/sashuu6/CET-MCA-S5-MiniProject-IOT_based_Water_Level_Indicator
 # Created on: 10/10/2019
 # Created by: Sashwat K
 # Last updateed: 10/10/2019
---
+++ src/sprinker-system/NodeMCU-sprinkler-code/NodeMCU-sprinkler-code.ino
@@ -0,0 +1,40 @@
+/*
+ * Projecr Name : IOT based water level indicator
+ * Project repository link : https://github.com/sashuu6/CET-MCA-S5-MiniProject-IOT_based_Water_Level_Indicator
+ * Author : Sashwat K
+ * Created on : 10 Oct 2019
+ * Last updated : 10 Oct 2019
+ * Microcontroller: Atmega 328p
+ * Pins used: Ultrasonic sensor:  Trigger pin - 4
+ *                                echo pin - 5
+ *            Touch pads(5 nos):  6,7,8,9,10
+ */
+
+/*************************************************/
+/*************************************************/
+// Class defined to handle water Level in the tank
+/*************************************************/
+/*************************************************/
+ class waterLevelDetection {
+  private:
+            int ultrasonicSensorTriggerPin = 4; // Ultrasonic sensor - trigger - digital pin 4
+            int ultasonicSensorEchoPin = 5; // Ultrasonic sensor - echo - digital pin 5
+            long duration; // Store duration from sending a pulse to receiving it
+            long waterLevelDistance; // Stores the distance between water and sensor in cm
+            // 5 touch pad pins
+            int touchPad1 = 6; // digital pin 6
+            int touchPad2 = 7; // digital pin 7
+            int touchPad3 = 8; // digital pin 8
+            int touchPad4 = 9; // digital pin 9
+            int touchPad5 = 10; // digital pin 10
+ };
+ 
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}

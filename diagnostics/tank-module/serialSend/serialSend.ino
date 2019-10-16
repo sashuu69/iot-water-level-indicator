c4402676b8293c5fe70b8c2c0a8b2095cd28e593
Sashwat K <sashwat0001@gmail.com>
2019-10-16 21:19:03 +0530

    Added diagnostics valve control

--- diagnostics/tank-module/serialSend/serialSend.ino
+++ diagnostics/tank-module/serialSend/serialSend.ino
@@ -1,9 +1,26 @@
+/*
+ * Projecr Name : IOT based water level indicator
+ * Project repository link : https://github.com/sashuu6/CET-MCA-S5-MiniProject-IOT_based_Water_Level_Indicator
+ * File name: serialSend.ino
+ * Author : Sashwat K
+ * Created on : 10 Oct 2019
+ * Last updated : 10 Oct 2019
+ * Microcontroller: Atmega 328p
+ */
+
+// Header files
+#include <Wire.h> // For serial communication
+#define SLAVE_ADDRESS 0x05 // Slave address for valve control
+ 
 void setup() {
   // put your setup code here, to run once:
-
+  Wire.begin(SLAVE_ADDRESS);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  Wire.write("*");
+  Wire.write("ttss");
+  Wire.write("#");
+  Wire.write("qqww");
 }
---
+++ diagnostics/valve-control-code/solenoidValve/solenoidValve.ino
@@ -0,0 +1,32 @@
+/*
+ * Projecr Name : IOT based water level indicator
+ * Project repository link : https://github.com/sashuu6/CET-MCA-S5-MiniProject-IOT_based_Water_Level_Indicator
+ * File name: NodeMCU-valve-control-code.ino
+ * Author : Sashwat K
+ * Created on : 10 Oct 2019
+ * Last updated : 10 Oct 2019
+ * Microcontroller : Atmega 328p
+ * Pins used: solenoid valve 1: 4
+ *            solenoid valve 2: 5
+ *            solenoid valve 3: 6
+ *            Moisure sensor: A0
+ */
+
+int tankSolenoidValve = 4;
+int sprinklerSolenoidValve = 5;
+int farmSolenoidValve = 6;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(tankSolenoidValve, OUTPUT);
+  pinMode(sprinklerSolenoidValve, OUTPUT);
+  pinMode(farmSolenoidValve, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(tankSolenoidValve, HIGH);
+  digitalWrite(sprinklerSolenoidValve, LOW);
+  digitalWrite(farmSolenoidValve, HIGH);
+  delay(500);
+}

250473d87d4a7d96baf07601f7efebaa40948932
Sashwat K <sashwat0001@gmail.com>
2019-10-17 19:32:54 +0530

    relay test diagnosis complete

--- diagnostics/main-board/relayTest/relayTest.py
+++ diagnostics/main-board/relayTest/relayTest.py
@@ -13,18 +13,13 @@ import RPi.GPIO as GPIO  # Import Raspberry Pi GPIO library
 from time import sleep  # Import the sleep function from the time module
 GPIO.setwarnings(False)  # Ignore warning for now
 GPIO.setmode(GPIO.BOARD)  # Use physical pin numbering
-# Set pin 7,11 to be an output pin and set initial value to low (off)
+# Set pin 7 to be an output pin and set initial value to low (off)
 GPIO.setup(7, GPIO.OUT, initial=GPIO.LOW)  # Relay 1
-GPIO.setup(11, GPIO.OUT, initial=GPIO.LOW)  # Relay 2
 i = 0
 while i < 3:  # Run forever
     GPIO.output(7, GPIO.HIGH)  # Turn on
     sleep(1)  # Sleep for 1 second
     GPIO.output(7, GPIO.LOW)  # Turn off
-    sleep(1)  # Sleep for 1 second
-    GPIO.output(11, GPIO.HIGH)  # Turn on
-    sleep(1)  # Sleep for 1 second
-    GPIO.output(11, GPIO.LOW)  # Turn off
     sleep(1)  # Sleep for 1 second
     i += 1
 print("Relay switching complete")

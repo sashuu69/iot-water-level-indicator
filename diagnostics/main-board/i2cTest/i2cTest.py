9d7ffa34b0d1a09f8643591be010772c7df0c932
Sashwat K <sashwat0001@gmail.com>
2019-10-17 23:50:16 +0530

    Added i2c diagnosis

---
+++ diagnostics/main-board/i2cTest/i2cTest.py
@@ -0,0 +1,17 @@
+"""
+ * Projecr Name : IOT based water level indicator
+ * Project repository link : https://github.com/sashuu6/CET-MCA-S5-MiniProject-IOT_based_Water_Level_Indicator
+ * File name: relayTest.py
+ * Author : Sashwat K
+ * Created on : 17 Oct 2019
+ * Last updated : 17 Oct 2019
+ * Microcontroller: Raspberry Pi Zero W
+ * Purpose: Checking I2C Communication
+"""
+import smbus
+import time
+
+bus = smbus.SMBus(1)
+address = 0x04
+number = bus.read_byte(address)
+print(number + "\n")

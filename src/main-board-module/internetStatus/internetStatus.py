72150fe1ac073500f6f53bd1c864fc0f7234f5ae
Sashwat K <sashwat0001@gmail.com>
2019-10-18 09:35:27 +0530

    working on main code

---
+++ src/main-board-module/internetStatus/internetStatus.py
@@ -0,0 +1,10 @@
+"""
+ * Projecr Name : IOT based water level indicator
+ * Project repository link : https://github.com/sashuu6/CET-MCA-S5-MiniProject-IOT_based_Water_Level_Indicator
+ * File name: internetStatus.py
+ * Author : Sashwat K
+ * Created on : 18 Oct 2019
+ * Last updated : 18 Oct 2019
+ * Microcontroller: Raspberry Pi Zero W
+ * Purpose: Checking connection to firebase.google.com
+"""

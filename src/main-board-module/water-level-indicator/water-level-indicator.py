efc321c10fe6727d049028e60dd72fbbaa1efa9b
Sashwat K <sashwat0001@gmail.com>
2019-10-18 09:57:03 +0530

    main-board-module pending

--- src/main-board-module/water-level-indicator/water-level-indicator.py
+++ src/main-board-module/water-level-indicator/water-level-indicator.py
@@ -1,12 +1,10 @@
-# Main process code
-# Project name: IOT based Water level indicator
-# Repository link: https://github.com/sashuu6/CET-MCA-S5-MiniProject-IOT_based_Water_Level_Indicator
-# Created on: 10/10/2019
-# Created by: Sashwat K
-# Last updateed: 10/10/2019
-
-import serial
-
-def tankCommunication(self):
-    
-    pass
+"""
+ * Projecr Name : IOT based water level indicator
+ * Project repository link : https://github.com/sashuu6/CET-MCA-S5-MiniProject-IOT_based_Water_Level_Indicator
+ * File name: water-level-indicator.py
+ * Author : Sashwat K
+ * Created on : 10 Oct 2019
+ * Last updated : 18 Oct 2019
+ * Microcontroller: Raspberry Pi Zero W
+ * Purpose: The main controller
+"""

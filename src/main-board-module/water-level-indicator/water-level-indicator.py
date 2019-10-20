613052eda2b7bd9a2e0204adaa4ab22cd463741f
Sashwat K <sashwat0001@gmail.com>
2019-10-20 12:30:29 +0530

    completed I2C communication

--- src/main-board-module/water-level-indicator/water-level-indicator.py
+++ src/main-board-module/water-level-indicator/water-level-indicator.py
@@ -9,15 +9,16 @@
  * Purpose: The main controller
 """
 
-import RPi_I2C_driver
-import os
-import subprocess
-from time import *
+import RPi_I2C_driver  # LCD driver
+import os  # for running bash commands
+import subprocess  # For returning bash commands
+from time import *  # for getting realtime
 
-tankModuleAdress = 0x05
-valveModuleAddress = 0x04
+tankModuleAdress = 0x05  # Arduino tank module address
+valveModuleAddress = 0x04  # Arduino valve cmodule address
 
 
+# Function to send values through I2C from Pi to arduino using bash
 def i2cSendCommand(address, value):
     try:
         bashCommand = 'i2cset -y 1 ' + str(address) + ' ' + str(value)
@@ -28,18 +29,21 @@ def i2cSendCommand(address, value):
     return False
 
 
+# Function to receive values through I2C from Pi to arduino using bash
 def i2cReceiveCommand(address, value):
-	try:
-		sendBashCommand = 'i2cset -y 1 ' + str(address) + ' ' + str(value)
-		os.system(sendBashCommand)
-		receiveBashCommand = 'i2cget -y 1 ' + str(address)
-		bashValue =  subprocess.Popen(receiveBashCommand, shell=True, stdout=subprocess.PIPE).stdout
-		fValue =  bashValue.read().strip().decode()
-		decimalValue = int(fValue,16)
-		return decimalValue
-	except:
-		pass
+    try:
+        sendBashCommand = 'i2cset -y 1 ' + str(address) + ' ' + str(value)
+        os.system(sendBashCommand)
+        receiveBashCommand = 'i2cget -y 1 ' + str(address)
+        bashValue = subprocess.Popen(
+            receiveBashCommand, shell=True, stdout=subprocess.PIPE).stdout
+        fValue = bashValue.read().strip().decode()
+        decimalValue = int(fValue, 16)
+        return decimalValue
+    except:
+        pass
 
-i2cSendCommand(valveModuleAddress, 2)
+
+i2cSendCommand(valveModuleAddress, 3)
 finalResult = i2cReceiveCommand(tankModuleAdress, 222)
 print(finalResult)

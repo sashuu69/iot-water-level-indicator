f142ae844b4fd2735c7a465b562a830d9bab1792
Sashwat K <sashwat0001@gmail.com>
2019-10-23 14:58:52 +0530

    diagnostics firebase code working

--- diagnostics/main-board/firebase-connection/firebase-connection.py
+++ diagnostics/main-board/firebase-connection/firebase-connection.py
@@ -8,15 +8,27 @@ config = {  # configuration for connection
 firebase = pyrebase.initialize_app(config)  # firebase connection object
 db = firebase.database()  # firebase database initialisation
 
-# To create and update values in db
+# To create and update sensor-values table
 db.child("sensor-values").update(
     {"water-tank-percentage": "70",
      "pump-status": "false",
      "moisure-percentage": "50",
      "garden-valve": "false",
      "tank-valve": "false",
-     "any-valve-open": "false"})
+     "any-valve-open": "false"}
+)
 
-# users = db.child("sensor-values").child("Moisure percentage").get()
+# To create and update log table
+db.child("log").child("2019/10/13").child("14:51:50").update(
+    {"action": "Pump on"}
+)
 
-# print(users.val())
+# To create and update users table
+db.child("users").update(
+    {"username": "vijitha",
+     "password": "password"}
+)
+
+# TO get values from table
+mosireValue = db.child("sensor-values").child("moisure-percentage").get()
+print(mosireValue.val())

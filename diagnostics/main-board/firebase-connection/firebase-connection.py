c90b999dfb3a8088f28a6d95e3bb13f2272a1cd0
Sashwat K <sashwat0001@gmail.com>
2019-10-25 00:16:08 +0530

    Added new diagnostics code for firebase

--- diagnostics/main-board/firebase-connection/firebase-connection.py
+++ diagnostics/main-board/firebase-connection/firebase-connection.py
@@ -12,31 +12,31 @@ firebase = pyrebase.initialize_app(config)  # firebase connection object
 db = firebase.database()  # firebase database initialisation
 
 # To create and update sensor-values table
-db.child("sensor-values").update(
-    {"water-tank-percentage": "70",
-     "pump-status": "false",
-     "moisure-percentage": "50",
-     "garden-valve": "false",
-     "tank-valve": "false",
-     "any-valve-open": "false",
-     "farm-valve": "false",
-     "farm-irrigation-time-on": "11:00:00",
-     "farm-irrigation-time-off": "11:15:00"}
-)
+# db.child("sensor-values").update(
+#     {"water-tank-percentage": "70",
+#      "pump-status": "false",
+#      "moisure-percentage": "50",
+#      "garden-valve": "false",
+#      "tank-valve": "false",
+#      "any-valve-open": "false",
+#      "farm-valve": "false",
+#      "farm-irrigation-time-on": "11:00:00",
+#      "farm-irrigation-time-off": "11:15:00"}
+# )
 
 # To create and update log table
-db.child("log").child("2019/10/13").child("10:15").update(
-    {"action": "Pump on"}
-)
-db.child("log").child("2019/10/13").child("10:30").update(
-    {"action": "Pump off"}
-)
+# db.child("log").child("2019/10/13").child("10:15").update(
+#     {"action": "Pump on"}
+# )
+# db.child("log").child("2019/10/13").child("10:30").update(
+#     {"action": "Pump off"}
+# )
 
 # To create and update users table
-db.child("users").update(
-    {"username": "vijitha",
-     "password": "password"}
-)
+# db.child("users").update(
+#     {"username": "vijitha",
+#      "password": "password"}
+# )
 
 while True:
     # TO get values from table
@@ -44,6 +44,7 @@ while True:
         "sensor-values").child("farm-irrigation-time-on").get().val()
     now = datetime.now()
     current_time = now.strftime("%H:%M")
-    timeFormat = timeForIrrigation.strftime("%H:%M")
+    timeFormat = datetime.strptime(timeForIrrigation, "%H:%M")
+    print(type(timeFormat))
     print(timeFormat)
     sleep(1)

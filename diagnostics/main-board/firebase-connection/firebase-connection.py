6b8f2924652ef0697a3d5acb6d4a7f04af4197eb
Sashwat K <sashwat0001@gmail.com>
2019-10-24 19:46:11 +0530

    working on date and time

--- diagnostics/main-board/firebase-connection/firebase-connection.py
+++ diagnostics/main-board/firebase-connection/firebase-connection.py
@@ -12,35 +12,37 @@ firebase = pyrebase.initialize_app(config)  # firebase connection object
 db = firebase.database()  # firebase database initialisation
 
 # To create and update sensor-values table
-db.child("sensor-values").update(
-    {"water-tank-percentage": "70",
-     "pump-status": "false",
-     "moisure-percentage": "50",
-     "garden-valve": "false",
-     "tank-valve": "false",
-     "any-valve-open": "false",
-     "farm-valve": "false",
-     "farm-irrigation-time": "11:00:00"}
-)
+# db.child("sensor-values").update(
+#     {"water-tank-percentage": "70",
+#      "pump-status": "false",
+#      "moisure-percentage": "50",
+#      "garden-valve": "false",
+#      "tank-valve": "false",
+#      "any-valve-open": "false",
+#      "farm-valve": "false",
+#      "farm-irrigation-time": "11:00:00"}
+# )
 
 # To create and update log table
-db.child("log").child("2019/10/13").child("14:51:50").update(
-    {"action": "Pump on"}
-)
+# db.child("log").child("2019/10/13").child("14:51:50").update(
+#     {"action": "Pump on"}
+# )
 
 # To create and update users table
-db.child("users").update(
-    {"username": "vijitha",
-     "password": "password"}
-)
+# db.child("users").update(
+#     {"username": "vijitha",
+#      "password": "password"}
+# )
 
 # TO get values from table
-timeForIrrigation = db.child(
-    "sensor-values").child("farm-irrigation-time").get().val()
+# timeForIrrigation = db.child(
+#     "sensor-values").child("farm-irrigation-time").get().val()
 
 while True:
+    timeForIrrigation = db.child(
+        "sensor-values").child("farm-irrigation-time").get().val()
     now = datetime.now()
-    current_time = now.strftime("%H:%M:%S")
+    current_time = str(now.strftime("%H:%M:%S"))
     print(current_time)
     if current_time == timeForIrrigation:
         print("Hello")
